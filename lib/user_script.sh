b4a37339adb0116e67ab44616795e7ba08cf3a76
Sam <github@nicholaslau.com>
2021-04-30 22:56:26 +1000

    first commit

---
+++ lib/user_script.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+curl -sL https://raw.githubusercontent.com/sdarwin/Ansible-VNC/master/scripts/quickinstall.sh | sudo bash && \
+cd /home/ubuntu/ && git clone https://github.com/flyingdrnick/pysel && \
+chown -R ubuntu:ubuntu pysel && cd pysel && chmod +x quickinstall.sh && chmod +x localinstall.sh && \
+pip3 install pyarmor
